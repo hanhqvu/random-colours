f0cb452fadcdc988a2a31878ab44de0117f873f3
hanhqvu <hanhqvu@users.noreply.github.com>
2022-12-23 13:21:08 +0900

    Initial Commit

---
+++ Random Colours/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Colours
+//
+//  Created by Hanh Vu on 2022/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
