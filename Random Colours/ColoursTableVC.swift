df31cf81f3ba29e4d6492161cdb39229b2a86e23
hanhqvu <hanhqvu@users.noreply.github.com>
2022-12-24 23:42:58 +0900

    feat: show random colour for each row

--- Random Colours/ColoursTableVC.swift
+++ Random Colours/ColoursTableVC.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 class ColoursTableVC: UIViewController {
-
+    
+    var colours: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addRandomColours()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "ToColoursDetailVC", sender: nil)
+    func addRandomColours() {
+        for _ in 0..<50 {
+            colours.append(createRandomColours())
+        }
     }
     
+    func createRandomColours() -> UIColor {
+        let randomColour = UIColor(red: CGFloat.random(in: 0...1),
+                                   green: CGFloat.random(in: 0...1),
+                                   blue: CGFloat.random(in: 0...1),
+                                   alpha: 1)
+        return randomColour
+    }
     /*
     // MARK: - Navigation
 
@@ -28,5 +39,23 @@ class ColoursTableVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension ColoursTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colours.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColourCell") else {
+            return UITableViewCell()
+        }
+       
+        cell.backgroundColor = colours[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "ToColoursDetailVC", sender: nil)
+    }
 }
